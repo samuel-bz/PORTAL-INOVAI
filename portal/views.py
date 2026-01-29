a64cfe810426ba0889f8312e6ef90031d581ccb4
LuizFHSs <devlfhss@gmail.com>
2026-01-29 14:48:34 -0300

    update-index.html

--- portal/views.py
+++ portal/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'news_posts': NewsPost.objects.all()
+    }
+    return render(request, 'index.html', context)
 
 def mulheres(request):
     return render(request, 'mulheres.html')
--- portal_inovai/settings.py
+++ portal_inovai/settings.py
@@ -119,3 +119,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'portal' / 'static'
 ]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'

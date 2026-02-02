9c9967e4d18466922bcaf897be0ac18f7ce1fd2a
bernardo352 <129432740+bernardo352@users.noreply.github.com>
2026-02-02 11:39:52 -0300

    botão de logout adicionado

--- portal/views.py
+++ portal/views.py
@@ -1,3 +1,4 @@
+from pyexpat.errors import messages
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.contrib.auth import logout
@@ -17,7 +18,7 @@ class PortalLoginView(LoginView):
 def logout_view(request):
     """Log out and redirect; no template."""
     logout(request)
-    return redirect(settings.LOGOUT_REDIRECT_URL)
+    return redirect('portal:index')
 
 def news_editor(request):
     return render(request, 'news_editor.html')

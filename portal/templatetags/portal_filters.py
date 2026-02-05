a7f146252c8356396b1d3a6ce48d8cd9232eeab1
LuizFHSs <devlfhss@gmail.com>
2026-02-05 09:07:52 -0300

    Alteracao-na-exibicao-das-tagas

---
+++ portal/templatetags/portal_filters.py
@@ -0,0 +1,11 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name='split')
+def split(value):
+    """
+    Returns the value turned into a list.
+    """
+    
+    return value.split(',')
